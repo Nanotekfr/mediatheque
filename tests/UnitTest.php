9f8261738e0d871dd21918d75267428b75a5dcf8
COUILLET Dylan <nanotek.developpeur@gmail.com>
2021-10-11 14:16:36 +0200

    ci/cd init

---
+++ tests/UnitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Adherent;
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+    public function testAdherent(): void
+    {
+      $adherent = new Adherent();
+      $adherent->setLastName('Toto');
+      $this->assertTrue($adherent->getLastName()=== 'Toto');
+    }
+}
