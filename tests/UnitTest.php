b781a4041ae1fbf0c8eb6b252f913fa3f54bac7d
COUILLET Dylan <nanotek.developpeur@gmail.com>
2021-10-27 14:46:30 +0200

    refactor arborescence

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -2,15 +2,12 @@
 
 namespace App\Tests;
 
-use App\Entity\Adherent;
 use PHPUnit\Framework\TestCase;
 
 class UnitTest extends TestCase
 {
-    public function testAdherent(): void
+    public function testSomething(): void
     {
-        $adherent = new Adherent();
-        $adherent->setLastName('Toto');
-        $this->assertTrue($adherent->getLastName() === 'Toto');
+        $this->assertTrue(true);
     }
 }
